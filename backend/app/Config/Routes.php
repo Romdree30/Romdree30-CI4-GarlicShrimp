f0829e729d91d4fdf68244eb2db4a75692e7699c
Romeo30 <romdreeb7645@gmail.com>
2025-11-12 23:04:03 +0800

    refactor(auth): Updated Auth Controller with following functions and expose Routes
    - Login = Auth::login
    - Logout = Auth::logout
    - Sign Up = Auth::signup

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -10,3 +10,6 @@ $routes->get('/login', 'Auth::login');
 $routes->get('/signup', 'Auth::signup');
 $routes->get('/moodboard', 'Users::moodboard');
 $routes->get('/roadmap', 'Users::roadmap');
+$routes->post('login', 'Auth::log_in');
+$routes->post('logout', 'Auth::logout');
+$routes->post('signup', 'Auth::sign_up');
