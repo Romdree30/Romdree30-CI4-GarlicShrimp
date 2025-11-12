76958f375cbf49fd3bdef1ece4ad6c56d0998d83
Romeo30 <romdreeb7645@gmail.com>
2025-11-12 17:40:07 +0800

    feat(requests): added a requests page

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -13,3 +13,4 @@ $routes->get('/roadmap', 'Users::roadmap');
 $routes->get('/dashboard', 'Admin::dashboard');
 $routes->get('/shrimpMenu', 'Admin::shrimpMenu');
 $routes->get('/accounts', 'Admin::accounts');
+$routes->get('/requests', 'Admin::requests');
