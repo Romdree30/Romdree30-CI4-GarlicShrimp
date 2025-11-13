18fbafbedb157bfd928a75cafea96efd2c572911
Romeo30 <romdreeb7645@gmail.com>
2025-11-13 13:45:17 +0800

    feat:(login): fix the login php code

--- backend/app/Controllers/auth.php
+++ backend/app/Controllers/auth.php
@@ -4,10 +4,4 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-class Auth extends BaseController
-{
-    public function login(): string
-    {
-        return view('auth/login');
-    }
-}
+class Auth extends BaseController {}
